0d57865ec2627aa87a094454e69f6cc057df605d
fairtrade0486 <86719148+fairtrade0486@users.noreply.github.com>
2026-09-16 18:38:52 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ const steps = [
 ];
 
 const APT_COMPLEXES = [
-  { name: "반도유보라 퍼스티지 아파트", dongStart: 101, dongEnd: 106 },
-  { name: "제일풍경채 디오션", dongStart: 101, dongEnd: 107 },
+  { name: "반도유보라 퍼스티지", dongStart: 101, dongEnd: 106 },
+  { name: "영종 제일풍경채 디오션", dongStart: 101, dongEnd: 107 },
   { name: "영종 오션파크 모아엘가 그랑데", dongStart: 611, dongEnd: 618 },
   { name: "영종베르힐 스카이시티", dongStart: 521, dongEnd: 533 },
 ];
